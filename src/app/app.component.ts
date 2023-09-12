fb05130a20f6a000424a2c294ac6754f641efc7e
SaintSteven <estebandiazaranda@gmail.com>
2023-09-12 18:48:12 -0300

    [cleanup] more cleaning and the html lang was changed to es

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,34 +6,5 @@ import { Product } from './models/product.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  listOfFood: Product[];
-  listOfDrinks: Product[];
 
-  productsSelectedArray: Product[];
-
-  constructor() {
-    this.productsSelectedArray = [];
-
-    this.listOfFood = [
-      new Product('Fries', 'https://cache-mcd-ecommerce.appmcdonalds.com/images/CL/380.png', 5000),
-      new Product('Burger', 'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kPXwtjrN/200/200/original?country=cl', 6000),
-      new Product('Taco', 'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$mc-burrito-a-la-mexican.png/200/200/original?country=mx', 7000),
-    ];
-
-    this.listOfDrinks = [
-      new Product('Soda', 'https://cache-mcd-ecommerce.appmcdonalds.com/images/CL/380.png', 5000),
-      new Product('Beer', 'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kPXwtjrN/200/200/original?country=cl', 6000),
-      new Product('Wine', 'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$mc-burrito-a-la-mexican.png/200/200/original?country=mx', 7000),
-    ];
-  }
-
-  clickProduct(product: Product) {
-    const productFinded = this.productsSelectedArray.find(p => p.name === product.name);
-    if (productFinded) {
-      productFinded.quantity++;
-    } else {
-      product.quantity = 1;
-      this.productsSelectedArray.push(product);
-    }
-  }
 }
